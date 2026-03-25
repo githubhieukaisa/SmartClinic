4cb6f71d9f915ee10b64aa84e8a7c183b390b2c6
githubhieukaisa <vuahoarong@gmail.com>
2026-03-25 20:45:09 +0700

    Sửa lấy userid

--- DTOs/GenerateTicketRequest.cs
+++ DTOs/GenerateTicketRequest.cs
@@ -1,11 +1,11 @@
-namespace SmartClinic.DTOs
+﻿namespace SmartClinic.DTOs
 {
     public class GenerateTicketRequest
     {
         public string PatientName { get; set; } = string.Empty;
         public string? PatientPhone { get; set; }
         public int DepartmentId { get; set; }
-        public bool PatientGender { get; set; } = true; // true = Nam, false = N?
+        public bool PatientGender { get; set; } = true; // true = Nam, false = Nữ
         public int? UserId { get; set; }
     }
 }
