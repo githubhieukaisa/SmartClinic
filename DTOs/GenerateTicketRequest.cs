6329e464e56b7e3c6e68721f75fe5066cb53a013
githubhieukaisa <vuahoarong@gmail.com>
2026-03-29 16:31:14 +0700

    Fix lỗi và sửa đăng ký

--- DTOs/GenerateTicketRequest.cs
+++ DTOs/GenerateTicketRequest.cs
@@ -1,14 +1,17 @@
+using SmartClinic.Constant;
+
 namespace SmartClinic.DTOs
 {
-        public class GenerateTicketRequest
-        {
-                public string PatientName { get; set; } = string.Empty;
-                public string? PatientPhone { get; set; }
-                public int DepartmentId { get; set; }
-                public int? DoctorShiftId { get; set; }
-                public bool PatientGender { get; set; } = true; // true = Nam, false = Nữ
-                public int? UserId { get; set; }
-                public bool IsEmergency { get; set; }
-                public bool IsPriority { get; set; }
-        }
+    public class GenerateTicketRequest
+    {
+        public string PatientName { get; set; } = string.Empty;
+        public string? PatientPhone { get; set; }
+        public int DepartmentId { get; set; }
+        public int? DoctorShiftId { get; set; }
+        public bool PatientGender { get; set; } = true; // true = Nam, false = Nữ
+        public int? UserId { get; set; }
+        public TicketStatus? StatusEnum { get; set; } = Constant.TicketStatus.Waiting;
+        public bool IsEmergency { get; set; }
+        public bool IsPriority { get; set; }
+    }
 }
